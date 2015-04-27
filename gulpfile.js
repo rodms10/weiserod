f9a6bfebd286ad3ab39b86e7c350cf1a58a6a50d
Rodrigo Silveira <rodrigos@outlook.com>
2015-04-26 23:44:15 -0700

    Autoprefixer & animated beer

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
+var autoprefixer = require('autoprefixer-core');
 var babelify = require('babelify');
 var browserify = require('browserify');
 var browserSync = require('browser-sync');
 var buffer = require('vinyl-buffer');
 var ghPages = require('gulp-gh-pages');
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
 var rimraf = require('rimraf');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 var watchify = require('watchify');
 var _ = require('lodash');
 
+
 var reload = browserSync.reload;
 
 var config = {
   entryJSFile: './src/js/page.js',
   outputJSDir: './dist/js/',
+  outputDir: './dist/',
 };
 
 function getBrowserifyBundle() {
@@ -38,6 +42,12 @@ gulp.task('js', function() {
     .pipe(reload({ stream: true }));
 });
 
+gulp.task('css', function () {
+  return gulp.src('./src/css/*.css')
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
 gulp.task('static', function() {
   gulp.src('./src/static/**')
     .pipe(gulp.dest('./dist/'));
@@ -49,14 +59,18 @@ gulp.task('clean', function(cb){
 });
 
 gulp.task('js:watch', function() {
-  gulp.watch(['./src/js/page.js'], ['js']);
+  gulp.watch(['./src/js/page.js'], ['js']).on('change', reload);
+});
+
+gulp.task('css:watch', function() {
+  gulp.watch(['./src/css/*.css'], ['css']).on('change', reload);
 });
 
 gulp.task('static:watch', function() {
-  gulp.watch(['./src/static/**'], ['static']);
+  gulp.watch(['./src/static/**'], ['static']).on('change', reload);
 });
 
-gulp.task('build', ['js', 'static']);
+gulp.task('build', ['js', 'static', 'css']);
 
 gulp.task('default', ['build'], function() {
   process.exit(0);
@@ -67,7 +81,7 @@ gulp.task('publish', ['build'], function() {
     .pipe(ghPages());
 });
 
-gulp.task('watch', ['build', 'js:watch', 'static:watch'], function() {
+gulp.task('watch', ['build', 'js:watch', 'static:watch', 'css:watch'], function() {
 
   browserSync({
     server: {
@@ -76,7 +90,7 @@ gulp.task('watch', ['build', 'js:watch', 'static:watch'], function() {
   });
 
   getBrowserifyBundle().on('update', function() {
-    gulp.start('build')
+    gulp.start('build');
   });
 });
 
