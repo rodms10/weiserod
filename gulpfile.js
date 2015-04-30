626149073d98005f2aa143422aaa83b3e9e99457
Rodrigo Silveira <rodrigos@outlook.com>
2015-04-30 11:15:25 -0700

    Add image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var buffer = require('vinyl-buffer');
 var exit = require('gulp-exit');
 var ghPages = require('gulp-gh-pages');
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var postcss = require('gulp-postcss');
 var rimraf = require('rimraf');
 var source = require('vinyl-source-stream');
@@ -13,18 +15,11 @@ var uglify = require('gulp-uglify');
 var watchify = require('watchify');
 var _ = require('lodash');
 
-
 var reload = browserSync.reload;
-
-var config = {
-  entryJSFile: './src/js/page.js',
-  outputJSDir: './dist/js/',
-  outputDir: './dist/',
-};
 
 function getBrowserifyBundle() {
   return  watchify(browserify(_.extend({
-    entries: config.entryJSFile,
+    entries: './src/js/page.js',
     debug: true,
     // defining transforms here will avoid crashing your stream
     transform: [babelify]
@@ -39,7 +34,7 @@ gulp.task('js', function() {
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe(uglify())
-    .pipe(gulp.dest(config.outputJSDir))
+    .pipe(gulp.dest('./dist/js/'))
     .pipe(reload({ stream: true }));
 });
 
@@ -49,14 +44,19 @@ gulp.task('css', function () {
     .pipe(gulp.dest('./dist/css/'));
 });
 
+gulp.task('img', function () {
+  return gulp.src('./src/img/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('./dist/img/'));
+});
+
 gulp.task('static', function() {
   gulp.src('./src/static/**')
     .pipe(gulp.dest('./dist/'));
-});
-
-// clean the output directory
-gulp.task('clean', function(cb){
-  rimraf(config.outputDir, cb);
 });
 
 gulp.task('js:watch', function() {
@@ -67,11 +67,20 @@ gulp.task('css:watch', function() {
   gulp.watch(['./src/css/*.css'], ['css']).on('change', reload);
 });
 
+gulp.task('img:watch', function() {
+  gulp.watch(['./src/img/**'], ['img']).on('change', reload);
+});
+
 gulp.task('static:watch', function() {
   gulp.watch(['./src/static/**'], ['static']).on('change', reload);
 });
 
-gulp.task('build', ['js', 'static', 'css']);
+// clean the output directory
+gulp.task('clean', function(cb){
+  rimraf('./dist', cb);
+});
+
+gulp.task('build', ['js', 'static', 'css', 'img']);
 
 gulp.task('default', ['build'], function() {
   process.exit(0);
@@ -83,7 +92,10 @@ gulp.task('publish', ['build'], function() {
     .pipe(exit());
 });
 
-gulp.task('watch', ['build', 'js:watch', 'static:watch', 'css:watch'], function() {
+gulp.task(
+  'watch',
+  ['build', 'js:watch', 'static:watch', 'css:watch', 'img:watch'],
+  function() {
 
   browserSync({
     server: {
